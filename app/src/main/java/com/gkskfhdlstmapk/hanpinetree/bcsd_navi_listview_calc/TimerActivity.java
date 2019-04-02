698e673f32db555e0517387265b940f681a79197
H S <gkskfhdlstm@naver.com>
2019-04-02 23:05:30 +0900

    timer ms 반영한 코드

--- app/src/main/java/com/gkskfhdlstmapk/hanpinetree/bcsd_navi_listview_calc/TimerActivity.java
+++ app/src/main/java/com/gkskfhdlstmapk/hanpinetree/bcsd_navi_listview_calc/TimerActivity.java
@@ -1,56 +1,144 @@
 package com.gkskfhdlstmapk.hanpinetree.bcsd_navi_listview_calc;
 
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TimerActivity extends AppCompatActivity {
-    Chronometer chronometer;
-    Button startBtn, pauseBtn, resetBtn;
-    long stopTime = 0;
+
+    TextView textView ;
+
+    Button start, pause, reset, lap ;
+
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+
+    Handler handler;
+
+    int Seconds, Minutes, MilliSeconds ;
+
+    ListView listView ;
+
+    String[] ListElements = new String[] {  };
+
+    List<String> ListElementsArrayList ;
+
+    ArrayAdapter<String> adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        chronometer = (Chronometer)findViewById(R.id.chronometer);
-        startBtn = (Button)findViewById(R.id.startBtn);
-        pauseBtn = (Button)findViewById(R.id.pauseBtn);
-        resetBtn = (Button)findViewById(R.id.resetBtn);
-//
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        textView = (TextView)findViewById(R.id.textView);
+        start = (Button)findViewById(R.id.button);
+        pause = (Button)findViewById(R.id.button2);
+        reset = (Button)findViewById(R.id.button3);
+        lap = (Button)findViewById(R.id.button4) ;
+        listView = (ListView)findViewById(R.id.listview1);
+
+        handler = new Handler() ;
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(adapter);
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime() + stopTime);
-                chronometer.start();
-                startBtn.setVisibility(View.GONE);
-                pauseBtn.setVisibility(View.VISIBLE);
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+
             }
         });
 
-        pauseBtn.setOnClickListener(new View.OnClickListener() {
+        pause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                stopTime = chronometer.getBase() - SystemClock.elapsedRealtime();
-                chronometer.stop();
-                startBtn.setVisibility(View.VISIBLE);
-                pauseBtn.setVisibility(View.GONE);
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+
             }
         });
 
-        resetBtn.setOnClickListener(new View.OnClickListener() {
+        reset.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
-                stopTime = 0;
-                chronometer.stop();
-                startBtn.setVisibility(View.VISIBLE);
-                pauseBtn.setVisibility(View.GONE);
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                textView.setText("00:00:00");
+
+                ListElementsArrayList.clear();
+
+                adapter.notifyDataSetChanged();
             }
         });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ListElementsArrayList.add(textView.getText().toString());
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
     }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
+
 }
