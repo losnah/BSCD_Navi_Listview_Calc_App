a18b06d72f830b28868079201e4efd23ff202e3f
H S <gkskfhdlstm@naver.com>
2019-04-03 10:15:01 +0900

    10초마다 ListView Update 기능 추가

---
+++ app/src/main/java/com/gkskfhdlstmapk/hanpinetree/bcsd_navi_listview_calc/StopwatchActivity.java
@@ -0,0 +1,13 @@
+package com.gkskfhdlstmapk.hanpinetree.bcsd_navi_listview_calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
